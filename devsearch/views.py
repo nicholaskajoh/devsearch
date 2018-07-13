ffdb9290cfe3c2cb1e5231969f7fd4604dc06c4f
Nicholas Kajoh <kajohterna@gmail.com>
2018-07-13 11:45:39 +0100

    add search

--- devsearch/views.py
+++ devsearch/views.py
@@ -1,5 +1,7 @@
 from devsearch import app
-from flask import render_template
+from flask import render_template, request
+from devsearch.models import *
+import re
 
 
 @app.route('/')
@@ -9,4 +11,37 @@ def index():
 
 @app.route('/search')
 def search():
-    return render_template('search.html', title='Search results...')
+    q = request.args.get('q', default=None)
+    results = None
+    if q != None and q.strip() != '':
+        q = re.sub(' +', ' ', q)
+        words = q.lower().split()
+        pipeline = [
+            {'$match': {'word': {'$in': words}}},
+            {'$lookup': {
+                'from': 'page',
+                'localField': 'page',
+                'foreignField': '_id',
+                'as': 'page',
+            }},
+            {'$unwind': '$page'},
+            {'$project': {
+                'page': 1,
+                'score': {
+                    '$sum': [
+                        {'$multiply': ['$page.pagerank', 0.3]},
+                        {'$multiply': ['$tf', '$idf', 0.7]},
+                    ],
+                },
+            }},
+            {'$sort': {'score': -1}},
+            {'$group': {'_id': None, 'pages': {'$addToSet': '$page'}}},
+        ]
+        result = list(Index.objects.aggregate(*pipeline))[0]
+    
+    return render_template(
+        'search.html',
+        title='Search pages...',
+        q=q,
+        pages=result['pages'],
+    )
